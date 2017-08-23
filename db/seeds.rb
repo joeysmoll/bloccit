6085ff9db69eec4eb1f9d87e3e2060e853ba7b6e
Joey Smoll <jsmoll91@gmail.com>
2017-08-23 23:46:40 +0000

    bloccit crud 3 assignment complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,7 @@
 
  # Create Posts
  50.times do
- # #1
    Post.create!(
- # #2
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
@@ -12,16 +10,24 @@
  posts = Post.all
  
  # Create Comments
- # #3
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
+   )
+ end
+ 
+ # Create Questions
+ 100.times do
+   Question.create!(
+     title: RandomData.random_sentence,
+     body: RandomData.random_paragraph,
+     resolved: false
    )
  end
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Question.count} questions created"
 
