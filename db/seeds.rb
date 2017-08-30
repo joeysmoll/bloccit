69594fd876a7f73057afeb0b79d1342fca79ce24
Joey Smoll <jsmoll91@gmail.com>
2017-08-30 00:57:21 +0000

    bloccit posts users complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
  require 'random_data'
  
+  # Create Users
+ 5.times do
+   User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+ 
  #Create Topics
  15.times do
   Topic.create!(
@@ -7,12 +17,13 @@
     description: RandomData.random_paragraph
    )
   end
-  topics = Topic all
+  topics = Topic.all
 
  # Create Posts
  50.times do
  
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample, 
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -21,16 +32,21 @@
  posts = Post.all
  
  # Create Comments
- # #3
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
  
+ user = User.first
+ user.update_attributes!(
+   email: 'jsmoll91@gmail.com',
+   password: 'helloworld'
+ )
+ 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
