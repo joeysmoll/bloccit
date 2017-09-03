8122ba09875b61603c164f0765af832af63a96f2
Joey Smoll <jsmoll91@gmail.com>
2017-09-03 21:41:50 +0000

    bloccit favorites assignment complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -30,4 +30,9 @@ class Post < ApplicationRecord
         update_attribute(:rank, new_rank)
     end
     
+    def create_favorite
+        Favorite.create(post: self, user: self.user)
+        FavoriteMailer.new_post(self).deliver_now
+    end
+    
 end
